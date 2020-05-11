a5cad577feeee69c3f8bff354ad81424fc8240c3
Oliver Ou <oliver.ou@sap.com>
2020-05-11 17:37:23 +0800

    exercise 1

--- src/main/java/com/sap/bulletinboard/ads/controllers/DefaultController.java
+++ src/main/java/com/sap/bulletinboard/ads/controllers/DefaultController.java
@@ -1,16 +1,20 @@
 package com.sap.bulletinboard.ads.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/")
+@RequestMapping(value = "/", produces = MediaType.TEXT_PLAIN_VALUE)
 @RestController
 public class DefaultController {
 
     @GetMapping
     public String get() {
-
-        return "ok";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Cloud Application:\n");
+        stringBuilder.append("Bulletinboard-ads\n");
+        stringBuilder.append("Oliver's exercise.");
+        return stringBuilder.toString();
     }
 }
